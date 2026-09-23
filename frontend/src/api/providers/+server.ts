b6596162fbd4f28cea9b819e38064d48daf53e09
977ADAM <977team.95@gmail.com>
2026-09-23 16:31:32 +0300

    Update +server.ts

--- frontend/src/api/providers/+server.ts
+++ frontend/src/api/providers/+server.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler } from './$types';
+import type { RequestHandler } from '$lib/types';
 import { proxyJson } from '$lib/server/python-api';
 
 export const GET: RequestHandler = ({ request }) => proxyJson(request, '/api/providers', 'GET');
