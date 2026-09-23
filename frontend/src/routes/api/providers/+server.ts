6372625438e707e76ffb8f56d91f674212341cdb
977ADAM <977team.95@gmail.com>
2026-09-23 16:11:04 +0300

    Delete +server.ts

--- frontend/src/routes/api/providers/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from './$types';
-import { proxyJson } from '$lib/server/python-api';
-
-export const GET: RequestHandler = ({ request }) => proxyJson(request, '/api/providers', 'GET');
-export const POST: RequestHandler = ({ request }) => proxyJson(request, '/api/providers', 'POST');
