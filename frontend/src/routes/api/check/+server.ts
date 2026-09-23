6834f56133069e46c3d7b98afcd953e5119b37fb
977ADAM <977team.95@gmail.com>
2026-09-23 17:49:17 +0300

    .

--- frontend/src/routes/api/check/+server.ts
+++
@@ -1,4 +0,0 @@
-import type { RequestHandler } from './$types';
-import { proxyJson } from '$lib/server/python-api';
-
-export const POST: RequestHandler = ({ request }) => proxyJson(request, '/api/check', 'POST');
--- frontend/src/routes/api/form/+server.ts
+++
@@ -1,4 +0,0 @@
-import type { RequestHandler } from './$types';
-import { proxyJson } from '$lib/server/python-api';
-
-export const GET: RequestHandler = ({ request }) => proxyJson(request, '/api/form', 'GET');
